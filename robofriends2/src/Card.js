5b6e8adfc86e664b073b6c0db3af388bcedfea6a
psi0nicjedi <clinton.west@childrensalon.com>
2023-01-14 19:52:31 +0000

    Corrected sizing for robot cards

--- robofriends2/src/Card.js
+++ robofriends2/src/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = (props) => {
     return (
         <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5' >
-            <img alt='Roboto' src={'https://robohash.org/${props.id}?100x100'} />
+            <img alt='Roboto' src={'https://robohash.org/${props.id}?size=100x100'} />
             <div>
                <h2>{props.name}</h2>
                <p>{props.email}</p> 
